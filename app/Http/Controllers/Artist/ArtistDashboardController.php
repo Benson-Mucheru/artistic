c301f218b20a8b3a8814f2d0323f1e947ae5a0ac
Benson-Mucheru <benmk300@gmail.com>
2025-07-21 18:12:19 +0300

    Removed Gate class facade on the controller methods

--- app/Http/Controllers/Artist/ArtistDashboardController.php
+++ app/Http/Controllers/Artist/ArtistDashboardController.php
@@ -14,44 +14,37 @@ use Illuminate\Support\Facades\Storage;
 class ArtistDashboardController extends Controller
 {
     public function dashboard($id) : View {
-        Gate::authorize('check-artist-url', $id);
         $artist = Artist::findOrFail($id);
         return view('artist.dash.dashboard', ['artistData' => $artist]);
     }
 
     public function music($id) : View {
-        Gate::authorize('check-artist-url', $id);
         $artist = Artist::findOrFail($id);
         $songs = $artist->songs;
         return view('artist.dash.music', ['artistData' => $artist, 'artistSongs' => $songs]);
     }
 
     public function fans($id) : View {
-        Gate::authorize('check-artist-url', $id);
         $artist = Artist::findOrFail($id);
         return view('artist.dash.fans', ['artistData' => $artist]);
     }
 
-    public function earning($id) : View {
-        Gate::authorize('check-artist-url', $id);
+    public function earnings($id) : View {
         $artist = Artist::findOrFail($id);
         return view('artist.dash.earning', ['artistData' => $artist]);
     }
 
     public function edit($id){
-        Gate::authorize('check-artist-url', $id);
         $artist = Artist::findOrFail($id);
         return view('artist.dash.profile-edit', ['artistData' => $artist]);
     }
 
     public function music_add($id){
-        Gate::authorize('check-artist-url', $id);
         $artist = Artist::findOrFail($id);
         return view('artist.dash.music-add', ['artistData' => $artist]);
     }
 
     public function music_upload(MusicUploadRequest $request, $id){
-        Gate::authorize('check-artist-url', $id);
         $artist = Artist::findOrFail($id);
         /* $attributes = $request->validated();
 
