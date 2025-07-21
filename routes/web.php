1e41f4c18716319becc35a4f91c4b908ea815cf6
Benson-Mucheru <benmk300@gmail.com>
2025-07-21 18:10:15 +0300

    Adds authorization at the route level for the artists routes  using can alias

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,13 @@ Route::post('/artist/logout', [ArtistLoginController::class, 'destroy']);
 Route::post('/send', [ArtistSignupController::class, 'store']);
 
 Route::middleware([CreatorLogin::class])->controller(ArtistDashboardController::class)->group(function(){
-    Route::get('/artist/{id}/dashboard' , 'dashboard');
-    Route::get('/artist/{id}/music' , 'music');
-    Route::get('/artist/{id}/fans' , 'fans');
-    Route::get('/artist/{id}/earnings' , 'earnings');
-    Route::get('/artist/{id}/edit' , 'edit');
-    Route::get('/artist/{id}/music-add' , 'music_add');
-    Route::post('/artist/{id}/music-upload' , 'music_upload');
+    Route::get('/artist/{id}/dashboard' , 'dashboard')->can('check-artist-url', 'id');
+    Route::get('/artist/{id}/music' , 'music')->can('check-artist-url', 'id');
+    Route::get('/artist/{id}/fans' , 'fans')->can('check-artist-url', 'id');
+    Route::get('/artist/{id}/earnings' , 'earnings')->can('check-artist-url', 'id');
+    Route::get('/artist/{id}/edit' , 'edit')->can('check-artist-url', 'id');
+    Route::get('/artist/{id}/music-add' , 'music_add')->can('check-artist-url', 'id');
+    Route::post('/artist/{id}/music-upload' , 'music_upload')->can('check-artist-url', 'id');
 });
 
 Route::get('/fan', [FanController::class, 'index']);
