706e09149e111b0ab176bbb0f4e22602530bb282
Benson-Mucheru <benmk300@gmail.com>
2025-07-22 16:32:50 +0300

    Changed the h1 title on the hero section

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,12 +1,15 @@
 <x-layouts.navigation>
         <div class="text-center">
-            <h1 class="text-9xl ">Welcome to Artistik</h1>
+            <h1 class="text-9xl">Your Music. Your Platform. Your Rules</h1>
+            <h2 class="text-4xl">Sell, share, and grow your fanbase on your own terms</h2>
             <p>Sign in as </p>
-            <x-nav.nav-link href="/artist/signup">
-                <button class="bg-desertblue border-1 border-desertblue p-2 rounded-full text-desertorange">Artist</button>
-            </x-nav.nav-link>
-            <x-nav.nav-link href="/fan">
-                <button class="p-2 rounded-full border-1 border-desertblue text-desertblue">Fan</button>
-            </x-nav.nav-link>
+            <div class="flex gap-4 justify-between max-w-1/2 mx-auto">
+                <x-nav.nav-link href="/artist/signup" class="w-full">
+                    <button class="bg-desertblue border-1 border-desertblue p-2 rounded-full text-desertorange w-full">Artist</button>
+                </x-nav.nav-link>
+                <x-nav.nav-link href="/fan" class="w-full">
+                    <button class="p-2 rounded-full border-1 border-desertblue text-desertblue w-full">Fan</button>
+                </x-nav.nav-link>
+            </div>
         </div>
 </x-layouts.navigation>  
