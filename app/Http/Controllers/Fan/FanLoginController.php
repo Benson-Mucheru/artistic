e4c0fb2187ceac68d7e6fe6c92ac2f98b1f13dc4
Benson-Mucheru <benmk300@gmail.com>
2025-08-01 10:30:32 +0300

    Successfully login the fan

--- app/Http/Controllers/Fan/FanLoginController.php
+++ app/Http/Controllers/Fan/FanLoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Fan;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Fan\FanLoginRequest;
+use App\Models\Fan;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
@@ -15,15 +16,31 @@ class FanLoginController extends Controller
         return view('fan.auth.login');
     }
 
-    public function store(FanLoginRequest $request){
+    public function store(FanLoginRequest $request): RedirectResponse{
         //validate
         $attributes = $request->validated();
-        //dd($attributes);
+
         //attempt
-        dd(Auth::guard('fan')->attempt($attributes));
+        if(!Auth::guard('fan')->attempt($attributes)){
+           return back()->withInput()->withErrors(['login_failed' => 'The credentials your provided do not much']);
+        }
+        //find fan id
+        $fanId = Fan::firstWhere('email', $attributes['email'])->id;
+
+        //regenerate session id
+        $request->session()->regenerate();
+
         //login
+        Auth::guard('fan')->loginUsingId($fanId);
 
         //redirect
-        
+        return dd('Successfully logined in');
+    }
+
+    public function destory (Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        redirect('/');
     }
 }
